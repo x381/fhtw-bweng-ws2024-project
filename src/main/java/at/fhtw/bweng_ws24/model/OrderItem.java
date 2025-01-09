c40f182b32688ceaf633c0e569b8fb4f1273d9ec
ichkq <149685312+ichkq@users.noreply.github.com>
2025-01-09 13:12:51 +0100

    OrderController, OrderDto, Order, OrderItem, OrderMapper, OrderReposi… (#3)
    OrderController, OrderDto, Order, OrderItem, OrderMapper, OrderRepository, OrderService
    ---------
    
    Co-authored-by: n2k <adewred1@gmail.com>

---
+++ src/main/java/at/fhtw/bweng_ws24/model/OrderItem.java
@@ -0,0 +1,26 @@
+package at.fhtw.bweng_ws24.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_items") // Explicitly specify table name
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String productId;
+
+    @Column(nullable = false)
+    private int quantity;
+}
