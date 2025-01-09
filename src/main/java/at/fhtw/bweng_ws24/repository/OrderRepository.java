c2d9ce3aab6a8b04a3de0a1dff83079b07d65212
Nanrehr <2003hernanlopezperez@gmail.com>
2025-01-09 11:29:19 +0100

    Kirill, now you are able to work in Orders

--- src/main/java/at/fhtw/bweng_ws24/repository/OrderRepository.java
+++ src/main/java/at/fhtw/bweng_ws24/repository/OrderRepository.java
@@ -13,5 +13,5 @@ import java.util.UUID;
 @Repository
 public interface OrderRepository extends CrudRepository<User, UUID> {
     List<Order> findAllOrders();
-    List<Order> findAllOrdersByUser(User user);
+    //List<Order> findAllOrdersByUser(User user);
 }
--- src/main/java/at/fhtw/bweng_ws24/service/OrderService.java
+++ src/main/java/at/fhtw/bweng_ws24/service/OrderService.java
@@ -1,26 +1,22 @@
 package at.fhtw.bweng_ws24.service;
 
-import at.fhtw.bweng_ws24.mapper.OrderMapper;
-import at.fhtw.bweng_ws24.model.Product;
+
 import at.fhtw.bweng_ws24.repository.OrderRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import at.fhtw.bweng_ws24.model.Order;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.UUID;
 
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final OrderMapper orderMapper;
+    //private final OrderMapper orderMapper;
 
-    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper, PasswordEncoder passwordEncoder) {
+    public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
-        this.orderMapper = orderMapper;
+        //this.orderMapper = orderMapper;
     }
 
     public List<Order> getOrders() {
